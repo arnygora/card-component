ad4f683cff9c8e453fe981170acb552fe80b513a
Anton Rozdobudko <arnygora@gmail.com>
2019-04-02 21:29:19 +0300

    add logo

--- src/components/socials/Card.js
+++ src/components/socials/Card.js
@@ -3,6 +3,11 @@ import {Author} from "../Author";
 
 const Card = () => (
     <div className="card-item">
+        <div className="logo-block">
+            <div className="logo-block__title">
+                <marquee scrollamount="2">DEV</marquee>
+            </div>
+        </div>
         <h1 className='card-item__title'>Learning React Start? Start Small</h1>
         <div className="author">
             <Author/>
