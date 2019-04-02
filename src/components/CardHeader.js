1eb51cd6fdb6e28dc013e21cd4368cedbd2a0510
Anton Rozdobudko <arnygora@gmail.com>
2019-04-03 00:28:57 +0300

    add button

--- src/components/CardHeader.js
+++ src/components/CardHeader.js
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const CardHeader = () => (
-    <div className='header-block'>
-        <div className="header-block__logo">
-            <p>DEV</p>
-        </div>
-        <div className='header-block__text'>
-            <div className="header-block__title">
-                <h4>The Practical Dev</h4><span>&#64;ThePracticalDev - Sep 10</span>
+class CardHeader extends Component {
+    state = {
+        isClicked: false
+    };
+
+    onClickButton = () => {
+        this.setState({
+            isClicked: !this.state.isClicked
+        })
+    };
+
+    render() {
+        return (
+            <div className='header-block'>
+                <div className="header-block__logo">
+                    <p>DEV</p>
+                </div>
+                <div className='header-block__text'>
+                    <div className="header-block__title">
+                        <h4>The Practical Dev</h4><span>&#64;ThePracticalDev - Sep 10</span>
+                    </div>
+                    <p>Learning React? Start Small</p>
+                    <address>&#123; author:<a href='#'>&#64;dceddia</a> &#125;</address>
+                </div>
+                <button onClick={this.onClickButton}>{
+                    this.state.isClicked ? String.fromCharCode(9650) : String.fromCharCode(9660)
+                }</button>
             </div>
-            <p>Learning React? Start Small</p>
-            <address>author:<a href='#'>&#64;dceddia</a></address>
-        </div>
-    </div>
-);
+        )
+    }
+}
 
 export {CardHeader}
