d6292d2531a1628933471fd4fe12763e5b7e8de9
Anton Rozdobudko <arnygora@gmail.com>
2019-04-27 21:14:04 +0300

    add redux

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter} from "react-router-dom";
-import {ViewPage} from "./components/ViewPage";
+import { Provider } from 'react-redux';
+import { store } from "./store/configureStore";
+import { BrowserRouter } from "react-router-dom";
+import { ViewPage } from "./components/ViewPage";
 
 import './styles/styles.scss'
 
+
 ReactDOM.render(
-    <BrowserRouter>
-        <ViewPage/>
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <ViewPage/>
+        </BrowserRouter>
+    </Provider>,
     document.getElementById("root"),
     () => console.log(`Поточний час: ${new Date().toLocaleTimeString()}`));
