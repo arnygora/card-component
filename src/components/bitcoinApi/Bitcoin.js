3acb3ed39053d76bbb87ff91fa2f75c8b89e94b5
Anton Rozdobudko <arnygora@gmail.com>
2019-05-03 22:32:34 +0300

    add api bitcoin

--- src/components/bitcoinApi/Bitcoin.js
+++ src/components/bitcoinApi/Bitcoin.js
@@ -1,22 +1,77 @@
 import React, {Component} from 'react';
 import {Bar, Line, Pie} from 'react-chartjs-2';
-import { data } from './data'
+
+// import {data} from './data'
 
 class Bitcoin extends Component {
+    state = {
+        description: '',
+        data: {
+            labels: ['uno', 'dos', 'tres'],
+            datasets: [{
+                label: 'Bitcoin API',
+                data: [45, 54, 58],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.5)',
+                    'rgba(54, 162, 235, 0.5)',
+                    'rgba(255, 206, 86, 0.5)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)'
+                ],
+                borderWidth: 1
+            }]
+        }
+    };
 
-    componentWillUnmount() {
-        this.destroyChart();
+    async componentDidMount() {
+        const fetchParams = {
+            method: 'GET',
+            mode: 'cors'
+        };
+
+        const url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+        const res = await fetch(url, fetchParams);
+        const out = await res.json();
+
+        const {USD, EUR, GBP} = out.bpi;
+        console.log(out);
+        await this.setState((prevState) => ({
+            ...prevState,
+            description: out.disclaimer,
+            labels: [`${USD.description}`, `${EUR.description}`, `${GBP.description}`]
+        }));
     }
+
+    static defaultProps = {
+        titleDisplay: true,
+        legendDisplay: true,
+        legendPosition: 'bottom'
+    };
+
+    // componentWillUnmount() {
+    //     this.destroyChart();
+    // }
 
     render() {
         return (
             <div className='chart'>
-
-                <Bar data={data}
-                    />
+                <Bar data={this.state.data}
+                     options={{
+                         title: {
+                             display: this.props.titleDisplay,
+                             text: `${this.state.description}`,
+                             fontSize: 25
+                         },
+                         legend: {
+                             display: this.props.legendDisplay,
+                             position: this.props.legendPosition
+                         }
+                     }}
+                />
             </div>
-
-
         )
     }
 }
--- src/components/bitcoinApi/data.js
+++ src/components/bitcoinApi/data.js
@@ -1,15 +1,23 @@
 export const data = {
-    labels: ['January', 'February', 'March'],
-    datasets: [
-        {
-            label: 'My First dataset',
-            backgroundColor: 'rgba(255,99,132,0.2)',
-            borderColor: 'rgba(255,99,132,1)',
-            borderWidth: 1,
-            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-            hoverBorderColor: 'rgba(255,99,132,1)',
-            data: [65, 59, 80, 81, 56, 55, 40]
-        }
-    ]
-};
+        // labels: [`${USD.description}`, `${EUR.description}`, `${GBP.description}`],
+        labels: ['wef', 'wefwef', 'wefweg'],
+        datasets: [{
+            // label: `${label}`,
+            // data: [`${USD.rate_float}`, `${EUR.rate_float}`, `${GBP.rate_float}`],
+            data: [54654,654,6546],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(255, 206, 86, 0.5)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }]
+
+
+    };
 
